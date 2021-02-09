1df3266a481dda338edc0f6984c566a93a968eee
postmind <max.menshenin.dev@yandex.ru>
2021-02-09 16:54:16 +0300

    added save

--- register.php
+++ register.php
@@ -7,12 +7,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Register</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-
 </head>
 <body>
-<h1>Register new user</h1>
-<label>Enter password</label>
+<form action="save_user.php" method="post"
+<h1>Register new user</h1><br>
+<label>Enter login: </label>
 <br>
-<input name="password"/>
+
 </body>
 </html>
