acf14dabbd09f58d5cf9469d0315cea5d9898f7e
postmind <max.menshenin.dev@yandex.ru>
2021-02-10 11:09:11 +0300

    added maxlength and some minor changes

--- register.php
+++ register.php
@@ -8,12 +8,14 @@
     <title>Register</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
+
 <body>
 <form action="save_user.php" method="post"
 <h1>Register new user</h1><br>
-<label>Login: </label><input name="login"><br>
-<label>Password: </label><input name="password"><br>
+<label>Login: </label><input type="text" name="login" maxlength="15"><br>
+<label>Password: </label><input type="password" name="password" maxlength="15"><br>
 <button type="submit" class="btn btn-primary">Register</button>
 </form
+
 </body>
 </html>
