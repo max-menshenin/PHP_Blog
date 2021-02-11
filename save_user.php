0230700368101511102bcc0a941d98169418e334
postmind <max.menshenin.dev@yandex.ru>
2021-02-11 10:57:13 +0300

    can't find db

--- save_user.php
+++ save_user.php
@@ -1,1 +1,30 @@
 <?php
+if (isset($_POST['login'])) { $login = $_POST['login']; if ($login == '') { unset($login);}}
+if (isset($_POST['password`'])) { $password = $_POST['password']; if ($password == '') { unset($password);}}
+if (empty($login) or empty($password))
+{
+    $login = stripslashes($login);
+    $login = htmlspecialchars($login);
+    $password = stripslashes($password);
+    $password = htmlspecialchars($password);
+    $login = trim($login);
+    $password = trim($password);
+    // подключаемся к базе
+    include ("bd.php");
+    $result = mysql_query("SELECT id FROM user", $db);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
