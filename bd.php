b9d0c24aefdffb5bd2ab2ada76088138c06dde58
postmind <max.menshenin.dev@yandex.ru>
2021-02-12 11:50:28 +0300

    reformatted

--- bd.php
+++ bd.php
@@ -1,3 +1,4 @@
 <?php
 $db = mysqli_connect("localhost", "root", "root", "blog");
 echo "hooray";
+?>
--- save_user.php
+++ save_user.php
@@ -1,3 +1,53 @@
 <?php
-$db = mysqli_connect_connect("localhost", "root", "root", "blog");
-echo "hooray";
+include "bd.php";
+
+if (isset($_POST['login'])) {
+    $login = $_POST['login'];
+    if ($login == '') {
+        unset($login);
+    }
+}
+
+if (isset($_POST['password'])) {
+    $password = $_POST['password'];
+    if ($password == '') {
+        unset($password);
+    }
+}
+
+if (empty($login) or empty($password))
+{
+    $login = stripslashes($login);
+    $login = htmlspecialchars($login);
+    $password = stripslashes($password);
+    $password = htmlspecialchars($password);
+    $login = trim($login);
+    $password = trim($password);
+
+
+
+
+
+
+
+
+
+
+
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
