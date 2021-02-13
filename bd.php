eff34075e38017c19003d0a524eb432026119a73
postmind <max.menshenin.dev@yandex.ru>
2021-02-13 17:52:47 +0300

    remastered

--- bd.php
+++ bd.php
@@ -1,4 +1,13 @@
 <?php
-$db = mysqli_connect("localhost", "root", "root", "blog");
-echo "hooray";
+$servername = "localhost";
+$username = "root";
+$pass = "root";
+$db = "blog";
+$conn = new mysqli($servername, $username, $pass, $db);
+
+if ($conn->connect_error){
+    die($conn->connect_error);
+}
+$conn->query("INSERT INTO `data` (`id`, `message`) VALUES (NULL, 'testymessage') ");
+echo "string";
 ?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Блог</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+</head>
+<body>
+<form action="bd.php" method="post">
+
+    <input type="text"><br>
+    <button type="submit" class="btn btn-primary">Primary</button>
+</form>
+</body>
+</html>
